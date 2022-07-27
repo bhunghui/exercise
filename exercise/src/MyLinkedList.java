769d7da2dac35465ff6e6c13884b8c1b49d7d998
ywh <ywh_mail@qq.com>
2022-07-27 20:05:52 +0800

    complete MyArrayList

--- exercise/src/MyLinkedList.java
+++ exercise/src/MyLinkedList.java
@@ -1,138 +1,138 @@
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-
-public class MyLinkedList<E> implements Iterable<E> {
-    // 虚拟头尾节点
-
-    // 双链表节点
-    private static class Node<E> {
-
-        Node(E val) {
-        }
-    }
-
-    // 构造函数初始化头尾节点
-    public MyLinkedList() {
-    }
-
-
-    /***** 增 *****/
-
-    public void addLast(E e) {
-
-    }
-
-    public void addFirst(E e) {
-      }
-
-    public void add(int index, E element) {
-
-        }
-    }
-
-    /***** 删 *****/
-
-    public E removeFirst() {
-
-    }
-
-    public E removeLast() {
-    }
-
-    public E remove(int index) {
-
-    }
-
-    /***** 查 *****/
-
-    public E get(int index) {
-
-    }
-
-    public E getFirst() {
-
-    }
-
-    public E getLast() {
-
-    }
-
-    /***** 改 *****/
-
-    public E set(int index, E val) {
-
-    }
-
-    /***** 其他工具函数 *****/
-
-    public int size() {
-        return size;
-    }
-
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    private Node<E> getNode(int index) {
-        checkElementIndex(index);
-        Node<E> p = head.next;
-        // TODO: 可以优化，通过 index 判断从 head 还是 tail 开始遍历
-        for (int i = 0; i < index; i++) {
-            p = p.next;
-        }
-        return p;
-    }
-
-    private boolean isElementIndex(int index) {
-        return index >= 0 && index < size;
-    }
-
-    private boolean isPositionIndex(int index) {
-        return index >= 0 && index <= size;
-    }
-
-    /**
-     * 检查 index 索引位置是否可以存在元素
-     */
-    private void checkElementIndex(int index) {
-        if (!isElementIndex(index))
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
-    }
-
-    /**
-     * 检查 index 索引位置是否可以添加元素
-     */
-    private void checkPositionIndex(int index) {
-        if (!isPositionIndex(index))
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
-    }
-
-    private void display() {
-        System.out.println("size = " + size);
-        for (Node<E> p = head.next; p != tail; p = p.next) {
-            System.out.print(p.val + " -> ");
-        }
-        System.out.println("null");
-        System.out.println();
-    }
-
-    @Override
-    public Iterator<E> iterator() {
-        return new Iterator<>() {
-            Node<E> p = head.next;
-
-            @Override
-            public boolean hasNext() {
-                return p != tail;
-            }
-
-            @Override
-            public E next() {
-                E val = p.val;
-                p = p.next;
-                return val;
-            }
-        };
-    }
-
-}
+//import java.util.Iterator;
+//import java.util.NoSuchElementException;
+//
+//public class MyLinkedList<E> implements Iterable<E> {
+//    // 虚拟头尾节点
+//
+//    // 双链表节点
+//    private static class Node<E> {
+//
+//        Node(E val) {
+//        }
+//    }
+//
+//    // 构造函数初始化头尾节点
+//    public MyLinkedList() {
+//    }
+//
+//
+//    /***** 增 *****/
+//
+//    public void addLast(E e) {
+//
+//    }
+//
+//    public void addFirst(E e) {
+//      }
+//
+//    public void add(int index, E element) {
+//
+//        }
+//    }
+//
+//    /***** 删 *****/
+//
+//    public E removeFirst() {
+//
+//    }
+//
+//    public E removeLast() {
+//    }
+//
+//    public E remove(int index) {
+//
+//    }
+//
+//    /***** 查 *****/
+//
+//    public E get(int index) {
+//
+//    }
+//
+//    public E getFirst() {
+//
+//    }
+//
+//    public E getLast() {
+//
+//    }
+//
+//    /***** 改 *****/
+//
+//    public E set(int index, E val) {
+//
+//    }
+//
+//    /***** 其他工具函数 *****/
+//
+//    public int size() {
+//        return size;
+//    }
+//
+//    public boolean isEmpty() {
+//        return size == 0;
+//    }
+//
+//    private Node<E> getNode(int index) {
+//        checkElementIndex(index);
+//        Node<E> p = head.next;
+//        // TODO: 可以优化，通过 index 判断从 head 还是 tail 开始遍历
+//        for (int i = 0; i < index; i++) {
+//            p = p.next;
+//        }
+//        return p;
+//    }
+//
+//    private boolean isElementIndex(int index) {
+//        return index >= 0 && index < size;
+//    }
+//
+//    private boolean isPositionIndex(int index) {
+//        return index >= 0 && index <= size;
+//    }
+//
+//    /**
+//     * 检查 index 索引位置是否可以存在元素
+//     */
+//    private void checkElementIndex(int index) {
+//        if (!isElementIndex(index))
+//            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+//    }
+//
+//    /**
+//     * 检查 index 索引位置是否可以添加元素
+//     */
+//    private void checkPositionIndex(int index) {
+//        if (!isPositionIndex(index))
+//            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+//    }
+//
+//    private void display() {
+//        System.out.println("size = " + size);
+//        for (Node<E> p = head.next; p != tail; p = p.next) {
+//            System.out.print(p.val + " -> ");
+//        }
+//        System.out.println("null");
+//        System.out.println();
+//    }
+//
+//    @Override
+//    public Iterator<E> iterator() {
+//        return new Iterator<>() {
+//            Node<E> p = head.next;
+//
+//            @Override
+//            public boolean hasNext() {
+//                return p != tail;
+//            }
+//
+//            @Override
+//            public E next() {
+//                E val = p.val;
+//                p = p.next;
+//                return val;
+//            }
+//        };
+//    }
+//
+//}
