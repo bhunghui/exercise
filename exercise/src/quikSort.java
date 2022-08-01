9e66723b7d86bef962823f75d53f2ba2690c26c1
ywh <ywh_mail@qq.com>
2022-08-01 15:02:57 +0800

    complete for quikSort

--- exercise/src/quikSort.java
+++ exercise/src/quikSort.java
@@ -8,42 +8,27 @@ public class quikSort {
         System.out.println("");
         quikSort(arr, 0, arr.length - 1);
         display(arr);
-        int[] arr2 = new int[] {4,2,8,1,8};
+        int[] arr2 = new int[] {31,2,8,1,8,24,7,10,99,0};
         quikSort(arr2, 0 ,arr2.length - 1);
         display(arr2);
     }
 
     public static void quikSort(int[] arr, int start, int end) {
-        int left = start, right = end;
-        if (start == end) {return;}
-        int standard = arr[arr.length >> 1];
-        while (left < right) {
-            while (arr[left] < standard) {
-                if (left < right) {
-                    left++;
-                }else{
-                    return;
-                }
-            }
-            while (arr[right] > standard) {
-                if (right > left) {
-                    right--;
-                }else {
-                    return;
-                }
-            }
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            if ( left < right) {
-                left++;
-                right--;
-            }else {
-                return;
+        if (start > end) {return;}
+        int standard = arr[start];
+        int i = start, j = end;
+        while (i != j) {
+            while (arr[j] >= standard && i < j) { j--; }
+            while (arr[i] <= standard && i < j) { i++; }
+            if (i != j) {
+                swap(arr, i, j);
+            } else {
+                swap(arr, start, i);
+                break;
             }
         }
-        quikSort(arr, start, left);
-        quikSort(arr, right, end);
+        quikSort(arr, start, i - 1);
+        quikSort(arr, i + 1, end);
     }
     public static void display(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
@@ -51,4 +36,11 @@ public class quikSort {
         }
         System.out.println("");
     }
+
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
+
